1b53b4674d7d1f76370416c48f54b1ef53f5514a
atherlnx <ather.azure@gmail.com>
2020-06-03 11:46:15 -0700

    bootcamp first

---
+++ src/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>This is my login component</h2>  
+      </div>
+    );
+  }
+}
+
+export default Login;
